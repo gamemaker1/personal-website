0b806f93889c477e7fc28eb362db44bb21287672
Vedant Kulkarni <vedant.kulkarni@students.iiit.ac.in>
2025-04-12 13:11:17 +0530

    feat: improve education ui and content

--- source/commands.js
+++ source/commands.js
@@ -152,72 +152,73 @@ export const skills = () => `
 `
 
 export const education = () => `
+  <div>
+    <a href="#school">school</a>
+    <a href="#junior-college">junior college</a>
+    <a href="#undergrad">undergrad</a>
+  </div>
+
+  <div><em><small>click on one of the above, or swipe through the timeline below.</small></em></div>
+
   <div class="timeline">
-    <p class="slide" onclick="event.target.scrollIntoView({ behavior: 'smooth' })">
-      i studied in wisdom world school, wakad from first to tenth standard.
-      i was a junior house prefect, and represented my school in inter-school
-      debates and public speaking competitions - most notably winning second
-      place for my speech at once such contest. i have participated but never
-      won any sports competition - i seem to have a knack for running away
-      from problems, not people :P <br /><br />
+    <p id="school" class="slide" onclick="event.target.scrollIntoView({ behavior: 'smooth' })">
+      i studied at wisdom world school, wakad from grade one to ten. i was a
+      junior house prefect, represented my school in inter-school debates and
+      public speaking (best i got was second place). i participated in quite a
+      few sports events, but i think i'm better at running away from problems,
+      not people :P <br /><br />
 
-      i got into computers in around sixth or seventh grade, when my dad
-      introduced me to linux: he told me a game i wanted to play (prince of
-      persia) was only available on linux, and got me to install ubuntu by
-      myself on an ancient pc that was catching dust at home.
+      my love for computers started somewhere in sixth or seventh grade when
+      my dad convinced me that a game i wanted to play only ran on linux. i
+      ended up installing ubuntu on a dusty old pc, and went down the linux
+      rabbit hole - even disassembling the pc, rebuilding it with a lego case,
+      and installing every distro on it, from ubuntu to gentoo. <br /><br />
+
+      eventually, i started coding for fun, took part in google code-in (and got
+      a cool t-shirt!), and did a bit of everything - from writing android apps,
+      to building custom roms for my phone, to writing a toy os in rust.
     </p>
 
-    <p class="slide" onclick="event.target.scrollIntoView({ behavior: 'smooth' })">
-      as my interest grew, i learnt more and more about linux and computers -
-      going so far as to taking apart the pc itself and building it back with
-      a lego case, and installing every linux distro under the sun on it, even
-      gentoo! soon i got into coding stuff up for the fun of it, was introduced
-      to open source software, and the rest is all up on github.com/gamemaker1
-      for everyone too see (: <br /><br />
+    <p id="junior-college" class="slide" onclick="event.target.scrollIntoView({ behavior: 'smooth' })">
+      during the lockdown, i built a project called dabbu - a knowledge platform
+      that let users treat data from multiple providers like simple folders.
+      it took me through api design, nlp, server architecture, and more. it
+      also led me to write my own node.js package - a text extractor for office
+      documents - which now gets 10k+ downloads a week. <br /><br />
 
-      i took part in google code-in, and got a t-shirt for my contributions.
-      i also tried my hand at a lot of different things - from compiling roms
-      for my phone to creating android apps and writing a toy os in rust.
+      i also started contributing to open source projects like refined
+      github, fpm, serve, express-rate-limit, and agent torch, and ended up
+      co-maintaining the last two :) <br /><br />
+
+      post lockdown, i prepared for jee for two years while continuing my open
+      source work. i contributed to sunbird rc (which backs india’s vaccine
+      certificate infra), explored verifiable credentials, and worked on
+      the beckn protocol – even got an honourable mention in a national ondc
+      hackathon. <br /><br />
+
+      i gave a few talks in 2024 too - one in the sunbird dpg tech fusion
+      conference, about my journey into open source - and one at the foss united
+      meetup in hyderabad about the work i was doing on agent torch.
     </p>
 
-    <p class="slide" onclick="event.target.scrollIntoView({ behavior: 'smooth' })">
-      during the lockdown, i started my own project - a knowledge platform
-      called dabbu which enabled users to access their information from multiple
-      providers (google drive, gmail, one drive, hard drives, etc.) as simple
-      files and folders. this took me on a deep dive into various topics -
-      from server architecture and api design, to nlp techniques, to knowledge
-      graphs. it also led to me creating another project - a text extractor for
-      office documents - which is downloaded over 10k times a week as of today.
+    <p id="undergrad" class="slide" onclick="event.target.scrollIntoView({ behavior: 'smooth' })">    
+      in the monsoon of 2024, i joined iiit hyderabad for an undergrad in
+      cs. i got elected to the student parliament and later as academics
+      secretary. with my friend aarnav pai, we revamped the entire mess system
+      in under a month, and made it open source too: gitlab.iiit.ac.in/messcom.
       <br /><br />
 
-      after that, i started contributing to other open source projects, like
-      express-rate-limit, and eventually ended up as a co-maintainer :)
-    </p>
-
-    <p class="slide" onclick="event.target.scrollIntoView({ behavior: 'smooth' })">
-      post lockdown, i spent two years studying for jee, all while continuing
-      to contribute as much as i could to open source projects. i worked on
-      sunbird rc - the registry and credentials system that backs india's
-      vaccine certificate system - and dived into verifiable presentations and
-      credentials. i also contributed to beckn, participating and winning an
-      honourable mention in an ondc national hackathon, helping them dockerise
-      their reference implementation and improving their developer docs. <br /><br />
-
-      i even gave a talk at the sunbird dpg tech fusion conference in pune in 2024
-      on my journey in open source software.
-    </p>
-
-    <p class="slide" onclick="event.target.scrollIntoView({ behavior: 'smooth' })">
-      and then, i joined iiit hyderabad as an undergraduate computer science
-      student. i was elected to the student parliament, and then to the position
-      of student academics secretary. <br /><br />
-
-      working alongside batchmate aarnav pai, we completely revamped the college's
-      mess system, switching from biometric authentication to qr codes, allowing
-      per-meal registrations while respecting the capacities of messes, adding an
-      easy to use admin interface, streamlining the billing process, all in 20 days
-      of programming and 10 days of testing. we even made it an open source project;
-      you can check out the source code at gitlab.iiit.ac.in/messcom.
+      re-making the mess system was quite the task - but in the end, we made
+      something we hope will last longer than our stay at college. our focus
+      was automation of the currently manual mess system - so we designed and
+      implemented something that would require almost no human intervention
+      from end to end. we switched out the biometric authentication for a qr
+      code system, created a new portal for registrations that had the menu,
+      rates, and built in checks for capacities. we created an admin dashboard
+      with detailed analytics, and streamlined the billing process. we also
+      implemented the qr verification systems ourselves, setting up kiosks
+      with scanners in each mess to ensure quick, easy and accurate availment
+      of meals.
     </p>
   </div>
 `
